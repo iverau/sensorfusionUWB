d27a2ca799079207694e4d6aef0ae9915112313d
iverau <iverau@stud.ntnu.no>
2022-01-25 13:24:37 +0100

    Fixed pathing issue

--- testGTSam.py
+++ testGTSam.py
@@ -2,7 +2,8 @@ import gtsam
 from DataSets.extractData import ROSData
 from gtsam.symbol_shorthand import X, L
 import numpy as np
-from .settings import DATASET_NUMBER
+from settings import DATASET_NUMBER
+from DataTypes.uwb_position import UWB_Ancors_Descriptor
 
 class GtSAMTest:
 
@@ -11,7 +12,7 @@ class GtSAMTest:
         self.dataset = ROSData(DATASET_NUMBER)
         isam_params = gtsam.ISAM2Params()
         self.isam = gtsam.ISAM2(isam_params)
-
+        self.uwb_positions = UWB_Ancors_Descriptor(DATASET_NUMBER)
         self.pose_variables = []
         self.landmarks_variables = {}
         self.initialize_graph()
@@ -36,17 +37,24 @@ class GtSAMTest:
 
     def add_UWB_to_graph(self, graph, uwb_measurement, pose_variable):
 
-        #TODO: Få inn UWB posisjonene som initial values
-        #TODO: Få lagt inn rett transformasjoner 
 
-        landmark = self.get_UWB_landmark(uwb_measurement)
+
+        landmark, initial_estimate = self.get_UWB_landmark(uwb_measurement)
         measurement_noise = gtsam.noiseModel.Diagonal.Sigmas([uwb_measurement.std])
         graph.add(gtsam.RangeFactor2D(pose_variable, landmark, uwb_measurement.range, measurement_noise))
 
+        if initial_estimate:
+            self.isam.update(graph, initial_estimate)
+
+
     def get_UWB_landmark(self, uwb_measurement):
+        initial_estimate = None
         if uwb_measurement.id not in self.landmarks_variables.keys():
             self.landmarks_variables[uwb_measurement.id] = L(len(self.landmarks_variables.keys()))
-        return self.landmarks_variables[uwb_measurement.id]
+            uwb_pos =  self.uwb_positions[uwb_measurement.id]
+            initial_estimate = gtsam.Values()
+            initial_estimate.insert(self.landmarks_variables[uwb_measurement.id],gtsam.Point2(uwb_pos.x, uwb_pos.y))
+        return self.landmarks_variables[uwb_measurement.id], initial_estimate
 
     def run(self):
         result = self.isam.calculateEstimate()
@@ -55,6 +63,13 @@ class GtSAMTest:
         graph = gtsam.NonlinearFactorGraph()
         imu_counter = 0
         for measurement in self.dataset.generate_measurements():
+
+            # Pre integrer states til man når en ny landmark
+            # Når man når ny landmark, sett initial value til den forrige staten pluss odometri resultatet
+            # Legg så inn landmarket i grafen
+            # Legge inn landmarks målinger helt til det kommer en IMU måling
+            # TODO: Få lagt inn rett transformasjoner 
+
             if measurement.measurement_type.value == "UWB":
                 #self.add_UWB_to_graph(graph, measurement, X1)
                 print("Imu counter", imu_counter)
