52c704159039924ec673759bfd2a857520b8d6b2
iverau <iverau@stud.ntnu.no>
2022-02-17 15:53:54 +0100

    Improved plots

--- Plotting/plot_gtsam.py
+++ Plotting/plot_gtsam.py
@@ -1,8 +1,7 @@
-
-from cProfile import label
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_horizontal_trajectory(pose_estimate, x_lim, y_lim, uwb_beacons, ground_truth):
+def plot_horizontal_trajectory(position_estimates, x_lim, y_lim, uwb_beacons, ground_truth):
     plt.suptitle("Horizontal trajectory")
     uwb_beacons = {7782220156096217088: [-0.545153  , -0.04282936,  1.49997155], 7782220156096217089: [-92.77930304,  -6.67807677,   0.91031529], 7782220156096217090: [-22.33896783, -22.76350421,   1.49969375], 7782220156096217091: [-55.43239422,  35.36695921,  -1.19977753], 7782220156096217092: [-82.20259541,   8.18208795,   0.91097657]}
     x_list = []
@@ -13,13 +12,13 @@ def plot_horizontal_trajectory(pose_estimate, x_lim, y_lim, uwb_beacons, ground_
 
     plt.scatter(x_list, y_list)
     plt.plot(ground_truth.gt_transelation[0], ground_truth.gt_transelation[1], color="red")
-    plt.plot(pose_estimate[:, 1], pose_estimate[:, 0], color="blue")
-    plt.xlabel("y [m]")
-    plt.ylabel("x [m]")
-    plt.legend(["estimate", "ground truth"])
+    plt.plot(position_estimates[:, 1], position_estimates[:, 0], color="blue")
+    plt.xlabel("East [m]")
+    plt.ylabel("North [m]")
+    plt.legend(["Ground truth", "Estimates", "UWB beacons"])
     plt.grid()
 
-def plot_horizontal_trajectory_old(pose_estimate, x_lim, y_lim, landmark_variables):
+def plot_horizontal_trajectory_old(position_estimates, x_lim, y_lim, landmark_variables):
     plt.suptitle("Horizontal trajectory")
     uwb_beacons = {7782220156096217088: [-0.545153  , -0.04282936,  1.49997155], 7782220156096217089: [-92.77930304,  -6.67807677,   0.91031529], 7782220156096217090: [-22.33896783, -22.76350421,   1.49969375], 7782220156096217091: [-55.43239422,  35.36695921,  -1.19977753], 7782220156096217092: [-82.20259541,   8.18208795,   0.91097657]}
     x_list = []
@@ -29,67 +28,66 @@ def plot_horizontal_trajectory_old(pose_estimate, x_lim, y_lim, landmark_variabl
         y_list.append(p[1])
 
     plt.scatter(x_list, y_list)
-    plt.plot(pose_estimate[:, 1], pose_estimate[:, 0], color="blue")
-    plt.xlabel("y [m]")
-    plt.ylabel("x [m]")
-    plt.legend(["estimate", "ground truth"])
+    plt.plot(position_estimates[:, 1], position_estimates[:, 0], color="blue")
+    plt.xlabel("East [m]")
+    plt.ylabel("North [m]")
+    plt.legend(["Ground truth", "Estimates", "UWB beacons"])
     plt.grid()
 
-def plot_position(pose_estimates, ground_truth, time_steps):
+def plot_position(position_estimates, ground_truth, time_steps):
     time_steps[1:] -= time_steps[1] - time_steps[0]
-    print(time_steps)
 
-    plt.suptitle("X Y Z")
+    plt.suptitle("Positions")
     plt.subplot(311)
-    plt.plot(time_steps[:-1], pose_estimates[:, 0])
+    plt.plot(time_steps[:-1], position_estimates[:, 0])
     plt.plot(ground_truth.time  , ground_truth.gt_transelation[0, :])
-    plt.legend(["estimate", "ground truth"])
+    plt.legend(["Estimate", "Ground truth"])
 
     plt.grid()
-    plt.ylabel("X")
+    plt.ylabel("X [m]")
 
     plt.subplot(312)
-    plt.plot(time_steps[:-1], pose_estimates[:, 1])
+    plt.plot(time_steps[:-1], position_estimates[:, 1])
     plt.plot(ground_truth.time  , ground_truth.gt_transelation[0, :])
-    plt.legend(["estimate", "ground truth"])
+    plt.legend(["Estimate", "Ground truth"])
     plt.grid()
-    plt.ylabel("Y")
+    plt.ylabel("Y [m]")
 
     plt.subplot(313)
-    plt.plot(time_steps[:-1], pose_estimates[:, 2])
+    plt.plot(time_steps[:-1], position_estimates[:, 2])
     plt.plot(ground_truth.time  , ground_truth.gt_transelation[0, :])
-    plt.legend(["estimate", "ground truth"])
+    plt.legend(["Estimate", "Ground truth"])
     plt.grid()
-    plt.ylabel("Z")
+    plt.ylabel("Z [m]")
 
     plt.tight_layout()
 
 
-def plot_angels(pose_estimates, ground_truth, time_steps):
+def plot_angels(euler_angels, ground_truth, time_steps):
     time_steps[1:] -= time_steps[1] - time_steps[0]
-    print(time_steps)
+    r2d = 180/np.pi
 
-    plt.suptitle("X Y Z")
+    plt.suptitle("Angels")
     plt.subplot(311)
-    plt.plot(time_steps[:-1], pose_estimates[:, 0])
-    plt.plot(ground_truth.time  , ground_truth.gt_angels[:, 0])
-    plt.legend(["estimate", "ground truth"])
+    plt.plot(time_steps[:-1], r2d * euler_angels[:, 0])
+    plt.plot(ground_truth.time  , r2d * ground_truth.gt_angels[:, 0])
+    plt.legend(["Estimate", "Ground truth"])
     plt.grid()
-    plt.ylabel("X")
+    plt.ylabel("Roll [deg]")
 
     plt.subplot(312)
-    plt.plot(time_steps[:-1], pose_estimates[:, 1])
-    plt.plot(ground_truth.time  , ground_truth.gt_angels[:, 1])
-    plt.legend(["estimate", "ground truth"])
+    plt.plot(time_steps[:-1], r2d * euler_angels[:, 1])
+    plt.plot(ground_truth.time  , r2d * ground_truth.gt_angels[:, 1])
+    plt.legend(["Estimate", "Ground truth"])
     plt.grid()
-    plt.ylabel("Y")
+    plt.ylabel("Pitch [deg]")
 
     plt.subplot(313)
-    plt.plot(time_steps[:-1], pose_estimates[:, 2])
-    plt.plot(ground_truth.time  , ground_truth.gt_angels[:, 2])
-    plt.legend(["estimate", "ground truth"])
+    plt.plot(time_steps[:-1], r2d * euler_angels[:, 2])
+    plt.plot(ground_truth.time, r2d * ground_truth.gt_angels[:, 2])
+    plt.legend(["Estimate", "Ground truth"])
     plt.grid()
-    plt.ylabel("Z")
+    plt.ylabel("Yaw [deg]")
 
     plt.tight_layout()
 
