04e519f45772b273b0ef987a4111b495dd54cda1
iverau <iverau@stud.ntnu.no>
2022-03-07 14:23:01 +0100

    Rotated IMU measurements, seems more stable

--- testUwbWithPreinitialization.py
+++ testUwbWithPreinitialization.py
@@ -69,7 +69,7 @@ class GtSAMTest:
 
         self.current_velocity = self.ground_truth.initial_velocity()
         self.current_bias = gtsam.imuBias.ConstantBias(np.zeros((3,)), np.array([-15e-3, -5e-3, -7e-3])) 
-        self.navstate = gtsam.NavState(self.current_pose.rotation(), self.current_pose.translation(), self.current_velocity)
+        self.navstate = gtsam.NavState(self.current_pose.rotation(), self.current_pose.translation(), self.current_pose.rotation().matrix().T @ self.current_velocity)
 
         self.factor_graph.add(gtsam.PriorFactorPose3(X1, self.current_pose, self.prior_noise_x))
         self.factor_graph.add(gtsam.PriorFactorVector(V1, self.current_velocity, self.prior_noise_v))
@@ -144,12 +144,11 @@ class GtSAMTest:
         )
 
         self.navstate = integrated_measurement.predict(self.navstate, self.current_bias)
-
         self.graph_values.insert(self.pose_variables[-1], self.navstate.pose())
-        self.graph_values.insert(self.velocity_variables[-1], self.current_velocity)
+        self.graph_values.insert(self.velocity_variables[-1], self.current_pose.rotation().matrix() @ self.navstate.velocity())
         self.graph_values.insert(self.imu_bias_variables[-1], self.current_bias)
 
-        self.factor_graph.add(gtsam.PriorFactorVector(self.velocity_variables[-1], self.navstate.velocity(), gtsam.noiseModel.Diagonal.Sigmas(np.array([0.001, 0.001, 0.001]))))
+        self.factor_graph.add(gtsam.PriorFactorVector(self.velocity_variables[-1], self.current_pose.rotation().matrix() @ self.navstate.velocity(), gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.1]))))
         self.factor_graph.add(gtsam.PriorFactorConstantBias(self.imu_bias_variables[-1], self.current_bias, self.prior_noise_b))
         self.factor_graph.add(gtsam.PriorFactorPose3(self.pose_variables[-1], self.navstate.pose(), gtsam.noiseModel.Diagonal.Sigmas(len(imu_measurements)*imu_measurements[0].variance_vector())))
 
@@ -240,8 +239,8 @@ class GtSAMTest:
                 self.current_pose = result.atPose3(self.pose_variables[-1]) 
                 self.current_velocity = result.atVector(self.velocity_variables[-1])
                 self.current_bias = result.atConstantBias(self.imu_bias_variables[-1])
-                self.navstate = gtsam.NavState(self.current_pose.rotation(), self.current_pose.translation(), self.current_velocity)
-                if len(self.pose_variables) > 1000:
+                self.navstate = gtsam.NavState(self.current_pose.rotation(), self.current_pose.translation(), self.current_pose.rotation().matrix().T @ self.navstate.velocity())
+                if len(self.pose_variables) > 3000:
                     break
 
 
