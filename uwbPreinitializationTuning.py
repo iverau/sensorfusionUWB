9a67262a15630df525c349772119d023729aff9b
iverau <iverau@stud.ntnu.no>
2022-03-10 14:41:38 +0100

    Very nice yaw plot

--- uwbPreinitializationTuning.py
+++ uwbPreinitializationTuning.py
@@ -10,12 +10,12 @@ DOWN_INITIAL_VALUE = -0.7
 BIAS_INITIAL_VALUE =  np.array([0, 0, 0, -15e-3, -5e-3, -7e-3])
 
 # UWB noises
-UWB_NOISE = 0.1
+UWB_NOISE = 0.5
 UWB_PRIOR_POSITIONING_NOISE = 1e-32
 
 # UWB-stage IMU tuning
-VELOCITY_SIGMAS = np.array([0.1, 0.1, 0.01]) # It is scalled with number of measurements
-POSE_SIGMAS = np.array([0.00175, 0.00175, 0.01, 1, 1, 0.001]) # It is scalled with number of measurements
+VELOCITY_SIGMAS = np.array([0.5, 0.5, 0.01]) # It is scalled with number of measurements
+POSE_SIGMAS = np.array([0.00175, 0.00175, 0.01, 2, 2, 0.001]) # It is scalled with number of measurements
 # pre-integration parameters can be found in the file Sensors/Imu.py
 
 #GNSS tuning
@@ -24,4 +24,4 @@ GNSS_NOISE = np.array([0.8, 0.8, 0.5, 2, 2, 5])
 GNSS_VELOCITY_SIGMAS = np.array([0.1, 0.1, 0.01])
 
 #Other constants
-NUMBER_OF_RUNNING_ITERATIONS = 1500 # Full traj is about 3000
+NUMBER_OF_RUNNING_ITERATIONS = 3000 # Full traj is about 3000
