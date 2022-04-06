f80d23e20feeb447b8564c9ab917acf58c5cd102
iverau <iverau@smint.no>
2022-04-06 13:32:16 +0200

    Starting on converting body transelation to NED

--- Sensors/CameraSensor2/visualOdometry.py
+++ Sensors/CameraSensor2/visualOdometry.py
@@ -9,15 +9,15 @@ def getCommonImagePoints(imageIndexes, kp1, kp2):
     kp1 = np.array(kp1)
     kp2 = np.array(kp2)
     imageIndexes = np.array(imageIndexes)
-    
-    relevantKp1 = kp1[imageIndexes[:,0]]
-    relevantKp2 = kp2[imageIndexes[:,1]]
+
+    relevantKp1 = kp1[imageIndexes[:, 0]]
+    relevantKp2 = kp2[imageIndexes[:, 1]]
 
     uv1 = []
     uv2 = []
     for k1, k2 in zip(relevantKp1, relevantKp2):
-        uv1.append([k1.pt[0], k1.pt[1],1])
-        uv2.append([k2.pt[0], k2.pt[1],1])
+        uv1.append([k1.pt[0], k1.pt[1], 1])
+        uv2.append([k2.pt[0], k2.pt[1], 1])
 
     return np.array(uv1), np.array(uv2)
 
@@ -36,11 +36,12 @@ def estimate_E_ransac(xy1, xy2, K, distance_threshold, num_trials):
     uv1 = K@xy1
     uv2 = K@xy2
 
-    print('Running RANSAC with %g inlier threshold and %d trials...' % (distance_threshold, num_trials), end='')
+    print('Running RANSAC with %g inlier threshold and %d trials...' %
+          (distance_threshold, num_trials), end='')
     best_num_inliers = -1
     for i in range(num_trials):
         sample = np.random.choice(xy1.shape[1], size=8, replace=False)
-        E_i = estimate_E(xy1[:,sample], xy2[:,sample])
+        E_i = estimate_E(xy1[:, sample], xy2[:, sample])
         d_i = epipolar_distance(F_from_E(E_i, K), uv1, uv2)
         inliers_i = np.absolute(d_i) < distance_threshold
         num_inliers_i = np.sum(inliers_i)
@@ -49,8 +50,10 @@ def estimate_E_ransac(xy1, xy2, K, distance_threshold, num_trials):
             E = E_i
             inliers = inliers_i
     print('Done!')
-    print('Found solution with %d/%d inliers' % (np.sum(inliers), xy1.shape[1]))
+    print('Found solution with %d/%d inliers' %
+          (np.sum(inliers), xy1.shape[1]))
     return E, inliers
+
 
 def F_from_E(E, K):
     K_inv = np.linalg.inv(K)
@@ -67,22 +70,21 @@ def epipolar_distance(F, uv1, uv2):
     l2 = F@uv1
     l1 = F.T@uv2
     e = np.sum(uv2*l2, axis=0)
-    norm1 = np.linalg.norm(l1[:2,:], axis=0)
-    norm2 = np.linalg.norm(l2[:2,:], axis=0)
+    norm1 = np.linalg.norm(l1[:2, :], axis=0)
+    norm2 = np.linalg.norm(l2[:2, :], axis=0)
     return 0.5*e*(1/norm1 + 1/norm2)
-
 
 
 def estimate_E(xy1, xy2):
     n = xy1.shape[1]
     A = np.empty((n, 9))
     for i in range(n):
-        x1,y1 = xy1[:2,i]
-        x2,y2 = xy2[:2,i]
-        A[i,:] = [x1*x2, y1*x2, x2, x1*y2, y1*y2, y2, x1, y1, 1]
+        x1, y1 = xy1[:2, i]
+        x2, y2 = xy2[:2, i]
+        A[i, :] = [x1*x2, y1*x2, x2, x1*y2, y1*y2, y2, x1, y1, 1]
 
-    _,_,VT = np.linalg.svd(A)
-    return np.reshape(VT[-1,:], (3,3))
+    _, _, VT = np.linalg.svd(A)
+    return np.reshape(VT[-1, :], (3, 3))
 
 
 def triangulate_many(xy1, xy2, P1, P2):
@@ -97,16 +99,17 @@ def triangulate_many(xy1, xy2, P1, P2):
             [shape 4 x n]
     """
     n = xy1.shape[1]
-    X = np.empty((4,n))
+    X = np.empty((4, n))
     for i in range(n):
-        A = np.empty((4,4))
-        A[0,:] = P1[0,:] - xy1[0,i]*P1[2,:]
-        A[1,:] = P1[1,:] - xy1[1,i]*P1[2,:]
-        A[2,:] = P2[0,:] - xy2[0,i]*P2[2,:]
-        A[3,:] = P2[1,:] - xy2[1,i]*P2[2,:]
-        U,s,VT = np.linalg.svd(A)
-        X[:,i] = VT[3,:]/VT[3,3]
+        A = np.empty((4, 4))
+        A[0, :] = P1[0, :] - xy1[0, i]*P1[2, :]
+        A[1, :] = P1[1, :] - xy1[1, i]*P1[2, :]
+        A[2, :] = P2[0, :] - xy2[0, i]*P2[2, :]
+        A[3, :] = P2[1, :] - xy2[1, i]*P2[2, :]
+        U, s, VT = np.linalg.svd(A)
+        X[:, i] = VT[3, :]/VT[3, 3]
     return X
+
 
 def decompose_E(E):
     """
@@ -115,19 +118,22 @@ def decompose_E(E):
 
     Returns a list of 4x4 transformation matrices.
     """
-    U,_,VT = np.linalg.svd(E)
+    U, _, VT = np.linalg.svd(E)
     R90 = np.array([[0, -1, 0], [+1, 0, 0], [0, 0, 1]])
     R1 = U @ R90 @ VT
     R2 = U @ R90.T @ VT
-    if np.linalg.det(R1) < 0: R1 = -R1
-    if np.linalg.det(R2) < 0: R2 = -R2
-    t1, t2 = U[:,2], -U[:,2]
-    return [SE3(R1,t1), SE3(R1,t2), SE3(R2, t1), SE3(R2, t2)]
+    if np.linalg.det(R1) < 0:
+        R1 = -R1
+    if np.linalg.det(R2) < 0:
+        R2 = -R2
+    t1, t2 = U[:, 2], -U[:, 2]
+    return [SE3(R1, t1), SE3(R1, t2), SE3(R2, t1), SE3(R2, t2)]
 
-def SE3(R,t):
+
+def SE3(R, t):
     T = np.eye(4)
-    T[:3,:3] = R
-    T[:3,3] = t
+    T[:3, :3] = R
+    T[:3, 3] = t
     return T
 
 
@@ -135,8 +141,9 @@ class VisualOdometry:
 
     def __init__(self, rot_init, t_init) -> None:
         self.camera = PinholeCamera()
-        self.detector = cv2.ORB_create(2000, 1.2, 8)
-        self.lk_params = dict(winSize  = (21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+        self.detector = cv2.ORB_create(1000, 1.2, 8)
+        self.lk_params = dict(winSize=(21, 21), criteria=(
+            cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
         self.old_image = None
         self.scale = 1.0
         self.n_features = 0
@@ -154,7 +161,8 @@ class VisualOdometry:
         self.Down = []
 
         # -90 grader rundt z, -90 grader i x
-        self.body_t_cam =  Rot.from_euler('xyz', [0.823, -2.807, 8.303], degrees=True).as_matrix() @ np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+        self.body_t_cam = Rot.from_euler(
+            'xyz', [0.823, -2.807, 8.303], degrees=True).as_matrix() @ np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
 
     def detect(self, img):
         points = self.detector.detect(img)
@@ -181,9 +189,10 @@ class VisualOdometry:
         image = np.array(image)
         # Track stuff
         if self.old_image is not None:
-            
+
             print("Start detector")
-            self.kp2, self.des2 = self.detector.detectAndCompute(self.old_image, None)
+            self.kp2, self.des2 = self.detector.detectAndCompute(
+                self.old_image, None)
             self.kp1, self.des1 = self.detector.detectAndCompute(image, None)
 
             print("Start matcher")
@@ -197,30 +206,29 @@ class VisualOdometry:
             print("Start outlier and estimate E")
             self.remove_outliers_with_ransac(imageIndexes)
             self.E = estimate_E(self.xy1, self.xy2)
-            
+
             # Start extrating T
             self.X, T = self.get_best_point_corespondence()
-            
+
             t = T[:3, 3]
             R = T[:3, :3]
             t = np.asarray([t]).T
 
-            # Transform the rotation from camera coordinates to 
-            print("Trans før: ",t, "\n")
+            # Transform the rotation from camera coordinates to
+            print("Trans før: ", t, "\n")
             print("Transformation:", R @ t, "\n")
 
-            #Kinematic equations for VO in NED
+            # Kinematic equations for VO in NED
             self.t += R @ t
             self.R = R.dot(self.R)
 
-            print("Trans etter: ",self.t)
-
+            print("Trans etter: ", self.t)
 
             rotation = Rot.from_matrix(self.body_t_cam.T @ self.R)
 
-            self.roll.append( rotation.as_euler("xyz", degrees=True)[0])
-            self.pitch.append( rotation.as_euler("xyz", degrees=True)[1])
-            self.yaw.append( rotation.as_euler("xyz", degrees=True)[2])
+            self.roll.append(rotation.as_euler("xyz", degrees=True)[0])
+            self.pitch.append(rotation.as_euler("xyz", degrees=True)[1])
+            self.yaw.append(rotation.as_euler("xyz", degrees=True)[2])
 
             self.North.append(self.body_t_cam.T.dot(self.t.copy())[0])
             self.East.append(self.body_t_cam.T.dot(self.t.copy())[1])
@@ -228,16 +236,15 @@ class VisualOdometry:
 
             print("Fort gjort")
 
-
             # Reset the variables to the new varaibles
             self.old_image = image
-            
+
             keypoints = self.detector.detect(image, None)
             keypoints, descriptors = self.detector.compute(image, keypoints)
-            new_img = cv2.drawKeypoints(image, keypoints, None, color=(0, 255, 0), flags=0)
+            new_img = cv2.drawKeypoints(
+                image, keypoints, None, color=(0, 255, 0), flags=0)
             cv2.imshow("Frame", new_img)
             cv2.waitKey(1)
-            
 
         else:
             # Case for first image
@@ -246,12 +253,13 @@ class VisualOdometry:
 
             # Initial rotation and transelation set to ground truth values
             self.R = self.body_t_cam @ self.initial_rotation
-            self.t =  self.body_t_cam @  self.initial_rotation @ np.asarray([self.initial_position]).T
+            self.t = self.body_t_cam @  self.initial_rotation @ np.asarray(
+                [self.initial_position]).T
 
             rotation = Rot.from_matrix(self.body_t_cam.T @ self.R)
-            self.roll.append( rotation.as_euler("xyz", degrees=True)[0])
-            self.pitch.append( rotation.as_euler("xyz", degrees=True)[1])
-            self.yaw.append( rotation.as_euler("xyz", degrees=True)[2])
+            self.roll.append(rotation.as_euler("xyz", degrees=True)[0])
+            self.pitch.append(rotation.as_euler("xyz", degrees=True)[1])
+            self.yaw.append(rotation.as_euler("xyz", degrees=True)[2])
 
             self.North.append(self.body_t_cam.T.dot(self.t.copy())[0])
             self.East.append(self.body_t_cam.T.dot(self.t.copy())[1])
@@ -260,7 +268,7 @@ class VisualOdometry:
             print("Intiial things", self.R)
 
     def update_scale(self):
-        # Update the scale parameter 
+        # Update the scale parameter
         pass
 
     def remove_outliers_with_ransac(self, imageIndexes):
